27091c42edbf4b93aec9411950b5518de5c97427
Diego Lopes <diegoplopes5@gmail.com>
2021-04-28 00:05:58 -0300

    feat: add contains to bst

--- 13_data_structures/06_binary_search_tree/01_bst.js
+++ 13_data_structures/06_binary_search_tree/01_bst.js
@@ -70,6 +70,35 @@ class BinarySearchTree {
         return findHelper(value, this.root);
 
     }
+
+    contains(value) {
+        if (!this.root) return false;
+
+        const containsHelper = (val, root) => {
+            if (root.value < val) {
+                if (root.right) {
+                    return containsHelper(val, root.right);
+                }
+
+                return false;
+
+            } else if (root.value > val) {
+                if (root.left) {
+                    return containsHelper(val, root.left);
+                }
+
+                return false;
+
+            } else if (val === root.value) {
+                return true;
+            }
+
+            return false;
+        }
+
+        return containsHelper(value, this.root);
+
+    }
 }
 
 const bst = new BinarySearchTree();
@@ -80,4 +109,8 @@ bst.insert(11)
 bst.insert(2)
 bst.insert(16)
 bst.insert(7)
+console.log(bst.find(2))
+console.log(bst.contains(2))
+
 console.log(bst.find(20))
+console.log(bst.contains(20))
