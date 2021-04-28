aef68cc7895975e89531d1409dfd237f51942053
Diego Lopes <diegoplopes5@gmail.com>
2021-04-27 23:51:07 -0300

    feat: add insert to bst

--- 13_data_structures/06_binary_search_tree/01_bst.js
+++ 13_data_structures/06_binary_search_tree/01_bst.js
@@ -10,4 +10,45 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+
+        const insertHelper = (node, root) => {
+            if (root.value < node.value) {
+                if (root.right) {
+                    return insertHelper(node, root.right);
+                }
+
+                root.right = node;
+                return this; 
+
+            } else if (root.value > node.value) {
+                if (root.left) {
+                    return insertHelper(node, root.left);
+                }
+
+                root.left = node;
+                return this;
+
+            }
+        }
+
+        return insertHelper(newNode, this.root)
+
+    }
 }
+
+const bst = new BinarySearchTree();
+bst.insert(10)
+bst.insert(5)
+bst.insert(13)
+bst.insert(11)
+bst.insert(2)
+bst.insert(16)
+bst.insert(7)
+console.log(bst)
