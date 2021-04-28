f3e21d6d8904d4fe3173b9d20be731c9074c668e
Diego Lopes <diegoplopes5@gmail.com>
2021-04-28 03:21:56 -0300

    feat: add remove

--- 13_data_structures/06_binary_search_tree/01_bst.js
+++ 13_data_structures/06_binary_search_tree/01_bst.js
@@ -100,12 +100,39 @@ class BinarySearchTree {
 
     }
 
+    remove(value) {
+        return BinarySearchTree.removeNode(this.root, value);
+    }
+
     successor(value) {
         return BinarySearchTree.findSuccessorNode(this.root, null, value);
     }
 
     predecessor(value) {
         return BinarySearchTree.findPredecessorNode(this.root, null, value);
+    }
+
+    static removeNode(root, value) {
+        if (!root) return null;
+
+        if (value < root.value) {
+            root.left = BinarySearchTree.removeNode(root.left, value);
+        } else if (value > root.value) {
+            root.right = BinarySearchTree.removeNode(root.right, value);
+        } else {
+            if(root.left && root.right) {
+                const rightMinNode = BinarySearchTree.findMinimumNode(root.right);
+                root.value = rightMinNode.value;
+                root.right = BinarySearchTree.removeNode(root.right, root.value);
+            } else if(root.left) {
+                root = root.left;
+            } else if(root.right) {
+                root = root.right;
+            } else {
+                root = null;
+            }
+        }
+        return root;
     }
 
     static findMinimumNode(node) {
@@ -150,16 +177,24 @@ class BinarySearchTree {
 }
 
 const bst = new BinarySearchTree();
-//              10
-//      5               13
-//  2       7       11      16
+const values = [10, 5, 15, 2, 7, 12, 17, 1, 3, 6, 8, 11, 13, 16, 18];
+values.forEach(value => bst.insert(value));
 
-bst.insert(10)
-bst.insert(5)
-bst.insert(13)
-bst.insert(11)
-bst.insert(2)
-bst.insert(16)
-bst.insert(7)
-console.log(bst.successor(7).value)
-console.log(bst.predecessor(7).value)
+// bst now :
+//                                  10
+//                  5                                   15
+//          2              7                    12              17
+//      1       3      6        8          11       13      16      18
+bst.remove(10);
+// bst now :
+//                                  11
+//                  5                                   15
+//          2              7                    12              17
+//      1       3      6        8                   13      16      18
+bst.remove(12);
+// bst now :
+//                                  11
+//                  5                                   15
+//          2              7                    13              17
+//      1       3      6        8                           16      18
+console.log(bst);
