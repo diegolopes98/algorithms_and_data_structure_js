aef89b29dc9f5eb04a15e33447e6d467bb1519b6
Diego Lopes <diegoplopes5@gmail.com>
2021-05-01 14:31:53 -0300

    feat: max heap extractMax

--- 13_data_structures/08_binary_heap/01_max_binary_heap.js
+++ 13_data_structures/08_binary_heap/01_max_binary_heap.js
@@ -7,8 +7,8 @@ class MaxBinaryHeap {
         this.values.push(value);
 
         const bubbleUp = (arr, currIdx = arr.length - 1) => {
-            const parentIdx = Math.floor((currIdx - 1)/2);
-            if(arr[currIdx] > arr[parentIdx]) {
+            const parentIdx = Math.floor((currIdx - 1) / 2);
+            if (arr[currIdx] > arr[parentIdx]) {
                 const tmp = arr[parentIdx];
                 arr[parentIdx] = arr[currIdx];
                 arr[currIdx] = tmp;
@@ -20,16 +20,54 @@ class MaxBinaryHeap {
 
         return this;
     }
+
+    extractMax() {
+        if (!this.values.length) return null;
+        if (this.values.length === 1) return this.values.pop();
+
+        const max = this.values[0];
+        const last = this.values.pop();
+        if (last) this.values[0] = last;
+
+        const bubbleDown = (arr, currIdx = 0) => {
+            if (!arr.length) return;
+
+            const leftChildIdx = 2 * currIdx + 1;
+            const rightChildIdx = 2 * currIdx + 2;
+
+            const curr = arr[currIdx];
+            const left = arr[leftChildIdx];
+            const right = arr[rightChildIdx];
+
+            if ((left && curr < left  ) || (right && curr < right)) {
+                if (left >= right) {
+                    arr[currIdx] = left;
+                    arr[leftChildIdx] = curr;
+                    return bubbleDown(arr, leftChildIdx);
+                } else {
+                    arr[currIdx] = right;
+                    arr[rightChildIdx] = curr;
+                    return bubbleDown(arr, rightChildIdx);
+                }
+            }
+
+        }
+
+        bubbleDown(this.values);
+
+        return max;
+    }
 }
 
 const maxHeap = new MaxBinaryHeap();
 
-maxHeap.insert(41);
-maxHeap.insert(39);
-maxHeap.insert(33);
-maxHeap.insert(18);
-maxHeap.insert(27);
-maxHeap.insert(12);
-maxHeap.insert(55);
+const values = [1, 3, 6, 5, 9, 8];
+values.forEach(value => maxHeap.insert(value));
 
+console.log(maxHeap.values);
+
+console.log(maxHeap.extractMax());
+console.log(maxHeap.values);
+
+console.log(maxHeap.extractMax());
 console.log(maxHeap.values);
